04d9a931a589f8f5a4c3719ab408eabc6d10b418
muzammil-tariq <muzammil413tariq@gmail.com>
2022-04-28 06:25:24 +0500

    upload contract

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+
+function getEth() {
+  // @ts-ignore
+  const eth = window.ethereum;
+  if (!eth) throw new Error("No Ethereum provider found");
+  return eth;
+}
+async function hasAccounts() {
+  const eth = getEth();
+  const accounts = (await eth.request({ method: "eth_accounts" })) as string[];
+  return accounts && accounts.length;
+}
+async function requestAccounts() {
+  const eth = getEth();
+  const accounts = (await eth.request({
+    method: "eth_requestAccounts",
+  })) as string[];
+  return accounts && accounts.length;
+}
+async function run() {
+  if (!(await hasAccounts()) && !(await requestAccounts()))
+    throw new Error("Let me take your money");
+  const hello = new ethers.Contract(
+    "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+    ["function hello() public pure returns (string memory)"],
+    new ethers.providers.Web3Provider(getEth())
+  );
+  document.body.innerHTML = await hello.hello();
+}
+run();
