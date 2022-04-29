4e847a357a51b70bd7e7a9303b3736a23f551202
muzammil-tariq <muzammil413tariq@gmail.com>
2022-04-29 15:30:37 +0500

    contract:add counter contract

---
+++ scripts/deploy-counter.ts
@@ -0,0 +1,13 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+async function deploy() {
+  const Counter = await ethers.getContractFactory("Counter");
+  const counter = await Counter.deploy();
+  await counter.deployed();
+  return counter;
+}
+async function count(counter) {
+  console.log("Counter", await counter.count());
+}
+deploy().then(count);
