16222c7f4add172dfb089e0eca0adfcf4eabe5c3
muzammil-tariq <muzammil413tariq@gmail.com>
2022-05-02 12:27:45 +0500

    gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter";
 module.exports = {
   solidity: "0.8.10",
   networks: {
